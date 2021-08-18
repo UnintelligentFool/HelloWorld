a44261b8e4bfd40e086d72c3dfaed4d3715d2170
UnintelligentFool <89090904+UnintelligentFool@users.noreply.github.com>
2021-08-18 11:29:28 +0200

    Inlämning - Niklas

---
+++ MyFirstApp/MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String myName = "Niklas Eriksson";
+
+            Console.WriteLine("Hello! My name is " + myName + ".");
+            //Console.ReadKey();
+
+        }
+    }
+}
